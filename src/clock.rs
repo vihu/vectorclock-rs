f0527a41871ab1076e07be3f6d222baed4e3b09d
Rahul Garg <rg@vihu.dev>
2020-06-11 21:31:37 -0700

    Allow cloning VectorClock, update test

--- src/clock.rs
+++ src/clock.rs
@@ -10,7 +10,7 @@ pub enum TemporalRelation {
     Concurrent,
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(PartialEq, Eq, Debug, Clone)]
 pub struct VectorClock<HostType: Hash + Eq> {
     entries: HashMap<HostType, u64>,
 }
@@ -174,13 +174,14 @@ mod test {
         assert_eq!(e, c.to_vec());
 
         let c = c.incremented("A");
+        let c = c.incremented("A");
 
-        assert_eq!(vec![("A", 1)], c.to_vec());
+        assert_eq!(vec![("A", 2)], c.to_vec());
 
         let c = c.incremented("B");
         let mut v = c.to_vec();
         v.sort();
 
-        assert_eq!(vec![("A", 1), ("B", 1)], v);
+        assert_eq!(vec![("A", 2), ("B", 1)], v);
     }
 }
